ac9d976e07927a926711313e0c62981d7db1ba60
De-la-torre <salomencarlos@gmail.com>
2022-04-07 22:40:53 -0500

    videojuego

---
+++ villa.js
@@ -0,0 +1,53 @@
+// var z;
+
+// for(var i=0; i<10; i++)
+// {
+//     z = aleatorio(10, 20);
+//     document.write(z + ", ")
+// }
+// document.write(z)
+var vp = document.getElementById("villaplatzi");
+var papel = vp.getContext("2d");
+var mapa = "tile.png";
+
+var fondo = new Image();
+fondo.src = mapa;
+fondo.addEventListener("load", dibujar);
+
+var vaca = new Image();
+vaca.src = "vaca.png";
+vaca.addEventListener("load", dibujarVacas);
+
+var pollo = new Image();
+pollo.src = "pollo.png";
+pollo.addEventListener("load", dibujarPollos)
+
+var cerdo = new Image();
+cerdo.src = "cerdo.png";
+cerdo.addEventListener("load", dibujarCerdos)
+
+function dibujar()
+{
+    papel.drawImage(fondo, 0, 0);
+}
+function dibujarVacas()
+{
+    papel.drawImage(vaca, 10, 10);
+}
+
+function dibujarPollos()
+{
+    papel.drawImage(pollo, 10, 300);
+}
+function dibujarCerdos()
+{
+    papel.drawImage(cerdo, 300, 150);
+}
+
+
+function aleatorio(min, maxi)
+{
+    var resultado;
+    resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+    return resultado;
+}

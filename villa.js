c1b7d81267a62fdf0ac69e50e44f5ac1dec2b515
De-la-torre <salomencarlos@gmail.com>
2022-04-08 22:16:27 -0500

    hello

--- villa.js
+++ villa.js
@@ -10,39 +10,69 @@ var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
 var mapa = "tile.png";
 
+var cantidad = aleatorio(1, 8);
+var cantidad1 = aleatorio(1, 3);
+var cantidad2 = aleatorio(1, 1)
+
 var fondo = new Image();
 fondo.src = mapa;
-fondo.addEventListener("load", dibujar);
+fondo.addEventListener("load", cargarFondos);
 
 var vaca = new Image();
 vaca.src = "vaca.png";
-vaca.addEventListener("load", dibujarVacas);
+vaca.addEventListener("load", cargarVacas);
 
 var pollo = new Image();
 pollo.src = "pollo.png";
-pollo.addEventListener("load", dibujarPollos)
+pollo.addEventListener("load", cargarPollos)
 
 var cerdo = new Image();
 cerdo.src = "cerdo.png";
-cerdo.addEventListener("load", dibujarCerdos)
+cerdo.addEventListener("load", cargarCerdos)
 
-function dibujar()
+
+function cargarFondos()
 {
     papel.drawImage(fondo, 0, 0);
 }
-function dibujarVacas()
+function cargarVacas()
 {
-    papel.drawImage(vaca, 10, 10);
+    console.log(cantidad);
+    for(var v=0; v < cantidad; v++)
+    {
+        var x = aleatorio(0, 5);
+        var y = aleatorio(0, 5);
+        var x = x * 60;
+        var y = y * 60;
+        papel.drawImage(vaca, x, y);
+    }
 }
 
-function dibujarPollos()
+function cargarPollos()
 {
-    papel.drawImage(pollo, 10, 300);
+    console.log(cantidad1);
+    for(var v=0; v < cantidad1; v++)
+    {
+        var x = aleatorio(0, 5);
+        var y = aleatorio(0, 5);
+        var x = x * 60;
+        var y = y * 60;
+        papel.drawImage(pollo, x, y);
+    }
 }
-function dibujarCerdos()
-{
-    papel.drawImage(cerdo, 300, 150);
+function cargarCerdos()
+{   
+    console.log(cantidad2);
+    for(var v=0; v < cantidad2; v++)
+    {
+        var x = aleatorio(0, 5);
+        var y = aleatorio(0, 5);
+        var x = x * 60;
+        var y = y * 60;
+        papel.drawImage(cerdo, x, y);
+    }
 }
+
 
 
 function aleatorio(min, maxi)
